24747b49092b748ae4da17225c265ee1b221f81d
Jan Lisse <jan.lisse@posteo.de>
2020-01-09 21:11:29 +0100

    update aws sdk

--- app/models/TemplateStorage.scala
+++ app/models/TemplateStorage.scala
@@ -1,16 +1,18 @@
 package models
 
-import java.io.File
-import java.io.InputStream
+import java.io.{File, InputStream}
 import java.util.UUID
-import com.amazonaws.services.s3.model.ObjectMetadata
-import com.amazonaws.services.s3.model.PutObjectRequest
-import com.amazonaws.services.s3.model.GetObjectRequest
-import com.amazonaws.services.s3.model.CannedAccessControlList
+
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import com.amazonaws.services.s3.model.{
+  CannedAccessControlList,
+  GetObjectRequest,
+  ObjectMetadata,
+  PutObjectRequest
+}
 import play.api.Play
-import com.amazonaws.auth.BasicAWSCredentials
 import play.api.Play.current
-import com.amazonaws.services.s3.AmazonS3Client
 
 trait TemplateStorage {
   def save(file: File, fileName: String, mimeType: String): String
@@ -36,14 +38,18 @@ class S3TemplateStorage extends TemplateStorage {
   }
 
   def load(key: String): InputStream = {
-    S3Configuration.amazonS3.getObject(new GetObjectRequest(S3Configuration.awsBucket, key)).getObjectContent
+    S3Configuration.amazonS3
+      .getObject(new GetObjectRequest(S3Configuration.awsBucket, key))
+      .getObjectContent
   }
 }
 
 object S3Configuration {
-  val awsBucket = Play.application.configuration.getString("aws.s3.bucket").getOrElse(throw new RuntimeException("aws.s3.bucket must be configured"))
-  val awsAccessKey = Play.application.configuration.getString("aws.access.key").getOrElse(throw new RuntimeException("aws.access.key must be configured"))
-  val awsSecretKey = Play.application.configuration.getString("aws.secret.key").getOrElse(throw new RuntimeException("aws.secret.key must be configured"))
-  val awsCredentials = new BasicAWSCredentials(awsAccessKey, awsSecretKey)
-  val amazonS3 = new AmazonS3Client(awsCredentials)
+  val awsBucket = Play.application.configuration
+    .getString("aws.s3.bucket")
+    .getOrElse(throw new RuntimeException("aws.s3.bucket must be configured"))
+  val amazonS3 = AmazonS3ClientBuilder
+    .standard()
+    .withCredentials(new DefaultAWSCredentialsProviderChain())
+    .build()
 }
--- build.sbt
+++ build.sbt
@@ -3,23 +3,28 @@ name := "dolorian"
 version := "1.0-SNAPSHOT"
 
 libraryDependencies ++= Seq(
-    jdbc,
-    anorm,
-    "org.webjars" %% "webjars-play" % "2.2.0",
-    "org.webjars" % "bootstrap" % "2.3.2",
-    "org.webjars" % "bootstrap-timepicker" % "0.2.3",
-    "org.webjars" % "requirejs" % "2.1.8",
-    "org.webjars" % "angularjs" % "1.1.5-1",
-    "org.postgresql" % "postgresql" % "9.4.1212",
-    "net.sf.jodreports" % "jodreports" % "2.4.0",
-    "com.amazonaws" % "aws-java-sdk" % "1.3.11",
-    "org.scalatest" % "scalatest_2.10" % "1.9.2" % "test",
-    "com.google.inject" % "guice" % "3.0",
-    "com.tzavellas" % "sse-guice" % "0.7.1"
- )
- 
+  jdbc,
+  anorm,
+  "org.webjars" %% "webjars-play" % "2.2.0",
+  "org.webjars" % "bootstrap" % "2.3.2",
+  "org.webjars" % "bootstrap-timepicker" % "0.2.3",
+  "org.webjars" % "requirejs" % "2.1.8",
+  "org.webjars" % "angularjs" % "1.1.5-1",
+  "org.postgresql" % "postgresql" % "9.4.1212",
+  "net.sf.jodreports" % "jodreports" % "2.4.0",
+  "com.amazonaws" % "aws-java-sdk-s3" % "1.11.702",
+  "org.scalatest" % "scalatest_2.10" % "1.9.2" % "test",
+  "com.google.inject" % "guice" % "3.0",
+  "com.tzavellas" % "sse-guice" % "0.7.1"
+)
+
 play.Project.playScalaSettings
 
 org.scalastyle.sbt.ScalastylePlugin.Settings
 
-scalacOptions ++= Seq("-unchecked", "-deprecation","-feature", "-language:postfixOps")
+scalacOptions ++= Seq(
+  "-unchecked",
+  "-deprecation",
+  "-feature",
+  "-language:postfixOps"
+)
